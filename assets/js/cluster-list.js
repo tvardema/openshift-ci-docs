93b9e75dfb6197be97607df48ccd284165dd7156
Hector Vido <hvidosil@redhat.com>
2026-07-30 08:55:57 -0300

    Modified the logic to consider hosted-mgmt with "String.includes"

--- assets/js/cluster-list.js
+++ assets/js/cluster-list.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
                         description = `${description} containing most Prow services.`;
                     } else if (item['cluster'] === 'core-ci') {
                         description = `${description} to replace app.ci (WIP).`;
-                    } else if (item['cluster'] === 'hosted-mgmt') {
+                    } else if (item['cluster'].includes('hosted-mgmt')) {
                         description = `${description} containing the Hosted Cluster control plane.`;
                     } else if (!item['cluster'].startsWith('build')) {
                         description = `${description} used for ${item['cluster'].replace(/\d+/g, '').toUpperCase()} tests, not managed by DPTP.`;
